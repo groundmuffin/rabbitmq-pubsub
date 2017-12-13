5793a03aacf7ec4b159cc978a8acc61992bcd6fb
groundmuffin <groundmuffin@mail.ru>
2017-12-14 00:01:04 +0200

    making subscriber queues autoDelete: true. (exclusive is not sufficient when using single connection)

--- src/subscriber.ts
+++ src/subscriber.ts
@@ -93,7 +93,8 @@ export class RabbitMqSubscriber {
 
     protected getQueueSettings(deadletterExchangeName: string): amqp.Options.AssertQueue {
         return {
-            exclusive: true
+            exclusive: true,
+            autoDelete: true
         }
     }
 
